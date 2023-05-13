cf307c20cde4a9bdaeea2b1d8b79bfffda0e3b1c
Joe <johnorjuela@yahoo.com>
2023-05-12 21:26:02 -0500

    Rutas

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ import "./database/connectdb.js";
 import express from 'express';
 const app = express();
 
-app.get("/", (req, res)=> {
-    console.log("Wiii");
-    res.json({ok:true});
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=> console.log("On fire http://localhost:"+PORT));
