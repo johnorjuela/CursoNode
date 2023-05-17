adf2e921c0e23b99ae810a9d5322642dae1d57b7
JOE <johnorjuela@yahoo.com>
2023-05-16 21:34:52 -0500

    Implementacion de Express Valdator

--- controllers/authcontrollers.js
+++ controllers/authcontrollers.js
@@ -1,9 +1,15 @@
+import { validationResult } from "express-validator";
 export const login = (req, res)=>{
     res.json({ok:"login",item:"perfecto"})
 };
 
 export const registrado = (req, res)=>{
     console.log(req.body);
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+
+    }
     res.json({ok:"registrado"});
 };
 
--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { login, registrado } from "../controllers/authcontrollers.js";
+import {body} from "express-validator";
 const router = express.Router();
 
 router.get("/login",login);
 
-router.post("/register",registrado);
+router.post("/register",[
+ body
+    ("email","Esto no es un Email").isEmail()
+ 
+],registrado);
 
 export default router;
